5eb2555af970398bafffe3a9696ba065063d6432
lazya <lazya@lazya.com>
2022-09-14 21:02:04 +0800

    first

---
+++ src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * Title: test
+ * Description:
+ *
+ * @author 懒闲人(ldg)
+ * @version 1.0
+ * @Date 2022 09 14 21 01
+ */
+public class test {
+    public void test(){
+        System.out.println("helloa");
+    }
+}
